c5ba26a98186ac1797a6f189f60dea8f8fb29f9f
Artem Huminskiy <huminskyi1325@gmail.com>
2017-04-17 23:31:12 +0300

    Fixed some mistakes in using flick api

--- app/src/main/java/com/android/huminskiy1325/photogallery/FlickrFetch.java
+++ app/src/main/java/com/android/huminskiy1325/photogallery/FlickrFetch.java
@@ -4,12 +4,19 @@ import android.net.Uri;
 import android.util.Log;
 import android.widget.Toast;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by cubru on 15.04.2017.
@@ -17,15 +24,17 @@ import java.net.URL;
 
 public class FlickrFetch {
 
-    public static final String ENDPOINT = "https://www.flickr.com/services/api/";
-    public static final String API_KEY = "0bc3f97c92abde09b64702b72a3005cb";
-    public static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
-    public static final String PARAM_EXTRAS = "extras";
-    public static final String EXTRA_SMALL_URL = "url_s";
+    private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
+    private static final String API_KEY = "0bc3f97c92abde09b64702b72a3005cb";
+    private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+    private static final String PARAM_EXTRAS = "extras";
+    private static final String EXTRA_SMALL_URL = "url_s";
 
-    public static final String TAG = "FlickrFetch";
+    private static final String XML_PHOTO = "photo";
 
-    byte[] getUrlBytes(String urlSpec) throws IOException {
+    private static final String TAG = "FlickrFetch";
+
+    private byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
         HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
@@ -53,7 +62,8 @@ public class FlickrFetch {
         return new String(getUrlBytes(urlSpec));
     }
 
-    public void fetchItem(){
+    public List<GalleryItem> fetchItem() {
+        List<GalleryItem> items = new ArrayList<>();
         try {
             String url = Uri.parse(ENDPOINT).buildUpon()
                     .appendQueryParameter("method", METHOD_GET_RECENT) //кодирует строки запросов
@@ -61,9 +71,41 @@ public class FlickrFetch {
                     .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
                     .build().toString();
             String xmlString = getUrl(url);
+            Log.i(TAG, "Received url " + url);
             Log.i(TAG, "Received xml " + xmlString);
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = factory.newPullParser();
+            parser.setInput(new StringReader(xmlString));
+
+            parseItems(items, parser);
+
         } catch (IOException e) {
-            Log.e(TAG, "Failed to fetch item",e);
+            Log.e(TAG, "Failed to fetch item", e);
+        } catch (XmlPullParserException xppe) {
+            Log.e(TAG, "Failed to parse items", xppe);
+        }
+
+        return items;
+    }
+
+    void parseItems(List<GalleryItem> items, XmlPullParser parser) throws XmlPullParserException, IOException {
+        int evenType = parser.next();
+
+        while (evenType != XmlPullParser.END_DOCUMENT) {
+            if (evenType == XmlPullParser.START_TAG && XML_PHOTO.equals(parser.getName())) {
+                String id = parser.getAttributeValue(null, "id");
+                String caption = parser.getAttributeValue(null, "title");
+                String smallUrl = parser.getAttributeValue(null, EXTRA_SMALL_URL);
+
+                GalleryItem item = new GalleryItem();
+                item.setCaption(caption);
+                item.setId(id);
+                item.setUrl(smallUrl);
+                items.add(item);
+            }
+
+            evenType = parser.next();
         }
     }
 }
